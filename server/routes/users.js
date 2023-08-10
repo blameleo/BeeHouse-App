de052cbf0b49d4b32e9835234ce7f9729b3db5f8
leonard <leonardgre@gmail.com>
2023-08-10 23:33:09 +0000

    done with backen logic for registering and login

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,18 +7,40 @@ const UserModel = require("../models/User.js");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, firstName, lastName, type } = req.body;
   const user = await UserModel.findOne({ email });
   if (user) {
     return res.json({ message: "user already exists!" });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new UserModel({ email, password: hashedPassword });
+  const newUser = new UserModel({
+    email,
+    password: hashedPassword,
+    firstName,
+    lastName,
+    type,
+  });
   await newUser.save();
   res.json({ message: "User registered successfully" });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return res.json({ message: "User doesnt exist" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or password is incorrect" });
+  }
+
+  const token = jwt.sign({ id: user._id }, "secret");
+
+  res.json({ token, userID: user._id });
+});
 
 module.exports.userRouter = router;
