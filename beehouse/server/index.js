ee17f96b049487ec220d3be509194f50ab790dea
leonard <leonardgre@gmail.com>
2023-09-12 21:27:41 +0000

    changes

--- beehouse/server/index.js
+++ beehouse/server/index.js
@@ -15,6 +15,12 @@ const port = 4000;
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(bodyParser.json());
 app.use(express.static("public"));
 // app.use("/images", express.static("images"));
